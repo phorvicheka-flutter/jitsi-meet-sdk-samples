1034311fb409af6fff9b606906125a7d977afd41
phorvicheka <phorvicheka@yahoo.com>
2024-04-24 18:29:34 +0900

    feat: learn how to make video call one to one, customize jitsi meet button (hide buttons and some feature flags)
    Notes:
    - use docker-jitsi-meet
    - use ngrok for https
    - jitsi-meet only work with secure https on mobile
    https://jitsi.github.io/handbook/docs/devops-guide/devops-guide-docker#configuration
    "The mobile apps won't work with self-signed certificates (the default). See below for instructions on how to obtain a proper certificate with Let's Encrypt."
    - video call only work with real device (video screen)
    - we can not customize buttons position of jitsi meet join/conference screen
    - (Optional) Package to be checked or consider:
    https://pub.dev/packages/jitsi_meet_wrapper
    
    Refs:
    https://github.com/jitsi/docker-jitsi-meet
    https://velog.io/@donggoo/HTTPS-HTTPS-%EC%82%AC%EC%84%A4-%EC%9D%B8%EC%A6%9D%EC%84%9C-%EB%B0%9C%EA%B8%89-%EB%B0%8F-%EA%B5%AC%ED%98%84-ngrok
    
    https://jitsi.github.io/handbook/docs/devops-guide/devops-guide-docker#configuration
    
    https://jitsi.github.io/handbook/docs/dev-guide/dev-guide-flutter-sdk
    https://github.com/jitsi/jitsi-meet/blob/master/config.js
    https://github.com/jitsi/jitsi-meet/blob/master/react/features/base/flags/constants.ts
    
    Troubleshooting
    ==============
    - Failed to load config
    https://github.com/jitsi/jitsi-meet-sdk-samples/issues/74
    You need a valid TLS certificate, the self-signed one won't work on mobile.
    
    - Ability to hide 'End meeting for all' button
    https://github.com/jitsi/jitsi-meet/issues/12653#issuecomment-1359113931
    If you are using docker-jitsi, add ENABLE_END_CONFERENCE=false in your env file.
    Otherwise follow this https://community.jitsi.org/t/how-to-disable-hangup-button-moderator-end-meeting-for-all/118415
    
    - Can no longer start a meeting without logging in via Google, Facebook, Microsoft
    https://github.com/jitsi/jitsi-meet/issues/13753#issuecomment-1694375462
    You can always self-host a deployment, installing it by default does not have any authentication.
    
    Authentication - Secure domain - JWT
    ===============================
    https://github.com/jitsi/lib-jitsi-meet/blob/master/doc/tokens.md
    https://jitsi.github.io/handbook/docs/devops-guide/devops-guide-docker#authentication
    https://jitsi.github.io/handbook/docs/devops-guide/secure-domain
    https://github.com/jitsi-contrib/jitok
    https://jitok.emrah.com/
    https://whatsmychaincert.com/
    https://github.com/FiloSottile/mkcert
    https://meetrix.io/blog/general/how-to-authenticate-users-to-Jitsi-meet-using-JWT-tokens.html

--- flutter/lib/main.dart
+++ flutter/lib/main.dart
@@ -30,33 +30,118 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final meetingNameController = TextEditingController();
+  final meetingNameController = TextEditingController(text: "UserName01");
   final jitsiMeet = JitsiMeet();
   void join() {
-
+    var listener = JitsiMeetEventListener(
+      conferenceJoined: (url) {
+        debugPrint("conferenceJoined: url: $url");
+      },
+      participantJoined: (email, name, role, participantId) {
+        debugPrint(
+          "participantJoined: email: $email, name: $name, role: $role, "
+          "participantId: $participantId",
+        );
+      },
+      participantLeft: (participantId) async {
+        debugPrint("participantLeft - participantId: $participantId");
+        jitsiMeet.retrieveParticipantsInfo();
+      },
+      participantsInfoRetrieved: (participantsInfo) {
+        debugPrint(
+            "participantsInfoRetrieved: participantsInfo: $participantsInfo");
+        // Extract the number of objects in the string
+        RegExp exp = RegExp(r'{[^}]*}');
+        int length = exp.allMatches(participantsInfo).length;
+        debugPrint(
+            "participantsInfoRetrieved: participantsInfo length: $length");
+        if (length == 1) {
+          jitsiMeet.hangUp();
+        }
+      },
+      readyToClose: () {
+        debugPrint("readyToClose");
+      },
+    );
     var options = JitsiMeetConferenceOptions(
-      serverURL: "https://meet.jit.si",
-      room: "test0987test",
+      // serverURL: "http://localhost:8000",
+      // serverURL: "https://localhost:8443",
+      // serverURL: "http://192.168.0.42:8000",
+      // serverURL: "https://192.168.0.42:8443",
+      serverURL: "https://22e4-118-44-27-227.ngrok-free.app",
+      // serverURL: "https://meet.jit.si",
+      room: "room01",
       configOverrides: {
-        "startWithAudioMuted": true,
-        "startWithVideoMuted": true,
-        "subject" : "JitsiwithFlutter",
-        "localSubject" : "localJitsiwithFlutter",
+        "startWithAudioMuted": false,
+        "startWithVideoMuted": false,
+        "hideConferenceSubject": true,
+        // "subject": "JitsiwithFlutter",
+        // "localSubject": "localJitsiwithFlutter",
+        // "prejoinConfig": {
+        //   "enabled": false,
+        // }
       },
       featureFlags: {
-        "unsaferoomwarning.enabled": false
+        // FeatureFlags.welcomePageEnabled: true,
+        // FeatureFlags.audioFocusDisabled: true,
+        FeatureFlags.addPeopleEnabled: false,
+        // FeatureFlags.audioMuteButtonEnabled: false,
+        FeatureFlags.audioOnlyButtonEnabled: false,
+        FeatureFlags.calenderEnabled: false,
+        FeatureFlags.callIntegrationEnabled: false,
+        // FeatureFlags.carModeEnabled: false,
+        FeatureFlags.closeCaptionsEnabled: false,
+        // FeatureFlags.conferenceTimerEnabled: false,
+        FeatureFlags.chatEnabled: false,
+        FeatureFlags.filmstripEnabled: false,
+        FeatureFlags.fullScreenEnabled: false,
+        FeatureFlags.helpButtonEnabled: false,
+        FeatureFlags.inviteEnabled: false,
+        FeatureFlags.androidScreenSharingEnabled: false,
+        FeatureFlags.speakerStatsEnabled: false,
+        FeatureFlags.kickOutEnabled: false,
+        FeatureFlags.liveStreamingEnabled: false,
+        FeatureFlags.lobbyModeEnabled: false,
+        FeatureFlags.meetingNameEnabled: false,
+        FeatureFlags.meetingPasswordEnabled: false,
+        FeatureFlags.notificationEnabled: false,
+        FeatureFlags.overflowMenuEnabled: false,
+        // FeatureFlags.pipEnabled: false,
+        // FeatureFlags.pipWhileScreenSharingEnabled: true,
+        FeatureFlags.preJoinPageEnabled: false,
+        // FeatureFlags.preJoinPageHideDisplayName: true,
+        FeatureFlags.raiseHandEnabled: false,
+        FeatureFlags.reactionsEnabled: false,
+        FeatureFlags.recordingEnabled: false,
+        // FeatureFlags.replaceParticipant: true,
+        FeatureFlags.securityOptionEnabled: false,
+        FeatureFlags.serverUrlChangeEnabled: false,
+        FeatureFlags.settingsEnabled: false,
+        FeatureFlags.tileViewEnabled: false,
+        // FeatureFlags.videoMuteEnabled: false,
+        FeatureFlags.videoShareEnabled: false,
+        // FeatureFlags.toolboxEnabled: false, // all buttons and conferenceTimerEnabled
+        FeatureFlags.toolboxAlwaysVisible: true,
+        // FeatureFlags.resolution: 'unset',
+        // FeatureFlags.unsafeRoomWarningEnabled: true,
+        // FeatureFlags.iosRecordingEnabled: true,
+        // FeatureFlags.iosScreenSharingEnabled: true,
+
+        "participants.enabled": false,
+        "breakout-rooms.enabled": false,
       },
-      userInfo: JitsiMeetUserInfo(
-          displayName: "Flutter user",
-          email: "user@example.com"
-      ),
+      // token:
+      //     "https://jwt.io/#debugger-io?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjb250ZXh0Ijp7InVzZXIiOnsiYXZhdGFyIjoiaHR0cHM6L2dyYXZhdGFyLmNvbS9hdmF0YXIvYWJjMTIzIiwibmFtZSI6IkpvaG4gRG9lIiwiZW1haWwiOiJqZG9lQGV4YW1wbGUuY29tIiwiaWQiOiJhYmNkOmExYjJjMy1kNGU1ZjYtMGFiYzEtMjNkZS1hYmNkZWYwMWZlZGNiYSJ9LCJncm91cCI6ImExMjMtMTIzLTQ1Ni03ODkifSwiYXVkIjoiaml0c2kiLCJpc3MiOiJteV9jbGllbnQiLCJzdWIiOiJtZWV0LmppdC5zaSIsInJvb20iOiIqIiwibmJmIjoyNTI0NjA4MDAwLCJleHAiOjI1MjQ2MDgwMDB9.WbtBTvYUOtx-VVLYJPja26H-Af8p-U70jJqmN6vk_Ds",
+      /* userInfo: JitsiMeetUserInfo(
+          displayName: "Flutter user", email: "user@example.com"), */
+      userInfo: JitsiMeetUserInfo(displayName: meetingNameController.text),
     );
-    jitsiMeet.join(options);
+    // jitsiMeet.hangUp();
+    jitsiMeet.join(options, listener);
   }
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
@@ -81,12 +166,12 @@ class _MyHomePageState extends State<MyHomePage> {
               width: 100,
               height: 50,
               child: FilledButton(
-                onPressed: join,
-                style: ButtonStyle(
-                  shape: MaterialStateProperty.all(RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0))),
-                ),
-                child: const Text("Join")
-              ),
+                  onPressed: join,
+                  style: ButtonStyle(
+                    shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10.0))),
+                  ),
+                  child: const Text("Join")),
             ),
           ],
         ),
