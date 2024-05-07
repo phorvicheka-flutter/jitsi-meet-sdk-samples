215e1a3a8bb7f484a7f92edf3e9eb55acad5fd6d
phorvicheka <phorvicheka@yahoo.com>
2024-05-07 14:31:03 +0900

    feat: implement caller side - jitsi meet joinRoom on success request api video call (/video/{loginId})
    Caller:
    - caller, click call, send request api video call
    - on success request, get roomName, create and join jitsi meet room
    
    Callee:
    - on received push FCM, show incoming call kit
    - on click accept button, join jitsi meeting with that room
    - on click reject button, do nothing (for now: just log)
    - on endcall, end incoming call kit and jitsi meeting

--- flutter/lib/constants/endpoints.dart
+++ flutter/lib/constants/endpoints.dart
@@ -1,5 +1,7 @@
 abstract class Endpoints {
   // static const String baseUrl = 'http://192.168.0.42:8000';
+  static const String jitsiMeetServerUrl =
+      'https://dev.connected-in.co.kr:8446';
   static const String baseUrl = 'https://dev.connected-in.co.kr/auth/api';
   static const int receiveTimeout = 15000;
   static const int connectionTimeout = 30000;
--- flutter/lib/data/enums/api_request_state.dart
+++ flutter/lib/data/enums/api_request_state.dart
@@ -1,1 +1,9 @@
 enum ApiRequestState { idle, loading, success, error }
+
+// https://stackoverflow.com/questions/38908285/how-do-i-add-methods-or-values-to-enums-in-dart
+extension ApiRequestStateExtension on ApiRequestState {
+  bool get isIdle => this == ApiRequestState.idle;
+  bool get isLoading => this == ApiRequestState.loading;
+  bool get isSuccess => this == ApiRequestState.success;
+  bool get isError => this == ApiRequestState.error;
+}
